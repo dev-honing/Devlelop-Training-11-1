046c3943a41d0f3e4014d7568f47db6a15e7dc22
Tech1997 <dev.honing@gmail.com>
2023-10-29 21:54:06 +0900

    validateEmail() 함수의 로직 작성

--- procedure.js
+++ procedure.js
@@ -1,1 +1,24 @@
-console.log("hello");
+// 이메일 유효성 검사
+// * 절차적 처리 예제
+function validateEmail(email) {
+  let hasAtSign = false;
+  let hasDot = false;
+
+  if (typeof(email) === "string") {
+    for (i=0; i < email.length; i++){
+      let char = email[i];
+      if (char === "@") {
+        hasAtSign = true;
+      }
+      if (char === ".") {
+        hasDot = true;
+      }
+    }
+  
+  
+  return hasAtSign && hasDot;
+
+} else {
+  console.log(email + "<- 이 매개변수는 올바른 데이터 타입이 아닙니다.")
+}
+}
