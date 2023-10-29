3d7c36bf89edb617261bec090fb69526d05faf66
Tech1997 <dev.honing@gmail.com>
2023-10-29 11:48:20 +0900

    '@'와 '.'을 필터링하는 로직 작성

--- app.js
+++ app.js
@@ -4,5 +4,16 @@ function validateEmail(email) {
   if (typeof(email) === "string") {
     // 문아열이면 배열로 변환
     const emailArray = Array.from(email);
-    }
+    
+    // '@'와 '.' 문자를 필터링
+    const filteredArray = emailArray.filter(char => {
+      if (char === "@") {
+        return true;
+      } else if (char === ".") {
+        return true;
+      } else {
+        return false;
+      }    
+    });
+}
 }
