391fe5fe484cdf9a33fdef7f5e0ecb251e27e71e
Tech1997 <dev.honing@gmail.com>
2023-10-29 11:45:43 +0900

    validateEmail함수의 조건식 로직 작성

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hello");
+// 이메일 유효성 검사
+// * 선언적 처리 예제
+function validateEmail(email) {
+  if (typeof(email) === "string") {
+    // 문아열이면 배열로 변환
+    const emailArray = Array.from(email);
+    }
+}
